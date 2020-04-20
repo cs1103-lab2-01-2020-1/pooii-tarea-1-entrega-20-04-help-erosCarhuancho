5c8f98ebac203fe3401f52ac21a413d681c4b50b
erosCarhuancho <55807262+erosCarhuancho@users.noreply.github.com>
2020-04-20 00:32:07 -0500

    stack.cpp

--- tarea_help/src/template/stack.cpp
+++ tarea_help/src/template/stack.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "stack.h"
+template <class T>
+bool Stack<T>::empty() const 
+{      
+  return vec.empty(); 
+} 
+
+template <class T>
+void Stack<T>::push (T const& elem) 
+{ 
+   vec.push_back(elem);    
+} 
+
+template <class T>
+T Stack<T>::top () const 
+{  
+  return vec.back();      
+} 
+
+
+template <class T>
+void Stack<T>::pop () { 
+    vec.pop_back();         
+} 
+
+
+template <class T>
+int Stack<T>::size()
+{
+  return vec.size();
+}
+
+
+
+
